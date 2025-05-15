c797dd611eb651c579c0db48e2b496553b5c1431
f1r3_r41n <qkrghkql1@gmail.com>
2025-05-15 17:37:44 +0900

    login server...

--- login_web_server/src/main.rs
+++ login_web_server/src/main.rs
@@ -1,11 +1,6 @@
-// actix-web = "4"
-// serde = { version = "1.0", features = ["derive"] }
-// serde_json = "1.0"
-// anyhow = "1.0"
-// tokio = { version = "1", features = ["macros", "rt-multi-thread"] }
-// sqlx = { version = "0.6", features = ["sqlite", "runtime-tokio-native-tls", "macros"] }
-// bcrypt = "0.12"
-// jsonwebtoken = "8"
+mod auth;
+mod routes;
+
 fn main() {
     println!("Hello, world!");
 }
